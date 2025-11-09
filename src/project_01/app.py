1e34f43975201fe1eafc7ee0bbcc2579345eef35
human02 <42168454+human02@users.noreply.github.com>
2025-11-08 20:59:54 -0800

    create simple login page

--- src/project_01/app.py
+++ src/project_01/app.py
@@ -1,29 +1,59 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, Response, session
 
 app = Flask(__name__)
+app.secret_key = "verysecret"
 
 
-@app.route("/", methods=["GET", "POST"])
+# Homepage
+@app.route("/", methods=["GET"])
 def home():
-    if request.method == "GET":
-        print(
-            f"\n{'#'*50}\nThis is the Home Page of the Website.\n\nWelcome! Hello World!\n"
-        )
-        return "Hello World, Flask with Poetry is up!"
-    if request.method == "POST":  # curl -X POST "http://127.0.0.1:5000/?name=John"
-        print(f"\nUser entered some data")
-        name = request.args.get("name", type=str)
-        return f"Thank you {name} for entering the details. Welcome {name}"
+    return f"""
+    <h2>Welcome to the Homepage</h2>
+    Click <a href={url_for("login")}>here</a> to login.
+    """
 
 
-@app.route("/about")
-def about():
-    return "This is the About US page."
-
-
-@app.route("/login")
+# - use for Login
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login Page"
+    session.pop("user", None)
+    if request.method == "GET":
+        return f"""
+                <h2> Login Page</h2>
+                <form method="POST">
+                <label>Username:</label>
+                        <input type="text" name="username" required>
+                        <br><br>
+                        <label>Password:</label>
+                        <input type="password" name="password" required>
+                        <br><br>
+                        <button type="submit">Login</button>
+                </form>
+                """
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # basic check
+        if username == "admin" and password == "12345":
+            session["user"] = username
+            return redirect(url_for("welcome"))
+        else:
+            return Response(
+                "Invalid Credentials, Please try again!", mimetype="text/plain"
+            )
+
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    if "user" in session:
+        return f"""
+        <h2>Welcome {session['user']}!</h2>
+        <p> Click below to logout </p>
+        <a href={url_for('login')}>Logout</a>
+        """
+    else:
+        return "Page not Found 404"
 
 
 if __name__ == "__main__":
